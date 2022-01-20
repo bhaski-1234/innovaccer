84e745335d5e7ec726cdc5a6467a8239cad1d1c1
siddthanth chopra <32588309+sidd2476@users.noreply.github.com>
2022-01-20 20:16:49 +0530

    Update __init__.py

--- api/market/__init__.py
+++ api/market/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 
 from flask_login import LoginManager
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SECRET_KEY']='d8c054f87c3a6142b8aea948'
